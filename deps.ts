6bc38b60e695374444aa1e25163835f41ccf2934
Kevin Liu <kevin07liu@gmail.com>
2022-03-05 21:26:28 +0800

    deps(Bson): add Bson form mongo

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { Application, Router } from "https://deno.land/x/oak@v10.4.0/mod.ts";
 
-export { MongoClient } from "https://deno.land/x/mongo@v0.29.2/mod.ts";
+export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.29.2/mod.ts";
 
 export {
   compareSync,
